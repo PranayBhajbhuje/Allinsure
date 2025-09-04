38654b6a6be8a10224fba78ae6c0f3ddb7424ffb
PranayBhajbhuje <pranaybhajbhuje266@gmail.com>
2025-09-04 16:19:56 +0530

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+
+document.addEventListener('click', (e) => {
+  const a = e.target.closest('a[href^="#"]');
+  if (a) {
+    const id = a.getAttribute('href').slice(1);
+    const el = document.getElementById(id);
+    if (el) {
+      e.preventDefault();
+      el.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+});
